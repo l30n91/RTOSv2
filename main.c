57da0708a909d14bf6205d959d3f42206bd24288
l30n491 <leonardo.buonaiuto91@gmail.com>
2025-09-21 15:39:21 +0200

    3 led blinking with 3 semaphores

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ static void TaskBlink(void *arg);
 static void TaskUART(void *arg);
 static osSemaphoreId_t sem1; // turno Task1
 static osSemaphoreId_t sem2; // turno Task2
-
+static osSemaphoreId_t sem3; // turno Task2
 
 
 static const osThreadAttr_t blinkAttr = {
@@ -39,6 +39,13 @@ void LED_blink_6(void) {
    GPIOA->ODR = (1U << 6);  // PA6 on
 }
 
+
+void LED_blink_7(void) {
+   //GPIOA->ODR ^= (1U << 6);  // PA6 on
+   GPIOA->ODR = (1U << 7);  // PA6 on
+}
+
+
 static void TaskBlink5(void *arg)
 {
   (void)arg;
@@ -59,12 +66,22 @@ static void TaskBlink6(void *arg)
     osSemaphoreAcquire(sem2, osWaitForever); // aspetta il turno
     LED_blink_6();
     osDelay(2000); 
-    osSemaphoreRelease(sem1);   // passa il turno a Task1 (intervento)    
+    osSemaphoreRelease(sem3);   // passa il turno a Task1 (intervento)    
   }
 } 
 
 
-
+static void TaskBlink7(void *arg)
+{
+  (void)arg;
+  for (;;)
+  {
+    osSemaphoreAcquire(sem3, osWaitForever); // aspetta il turno
+    LED_blink_7();
+    osDelay(2000); 
+    osSemaphoreRelease(sem1);   // passa il turno a Task1 (intervento)    
+  }
+} 
 
 
 int osKernelConfigStatus;
@@ -75,7 +92,7 @@ int main (void)
     GPIOA->MODER &= ~0x00000C00;    /* clear pin mode */
     GPIOA->MODER |=  0x00000400;    /* set pin to output mode */
     GPIOA->MODER |=  (1U << (6 * 2));  // set PA6 come output
-
+    GPIOA->MODER |=  (1U << (7 * 2));  // set PA6 come output
     USART2_init();                  /* initialize USART2 */
     
     
@@ -97,10 +114,12 @@ int main (void)
   osKernelInitialize();
   sem1 = osSemaphoreNew(1, 1, NULL);
   sem2 = osSemaphoreNew(1, 0, NULL);
+  sem3 = osSemaphoreNew(1, 0, NULL);
      
      
   osThreadNew(TaskBlink5, NULL, &blinkAttr);  // 2) crea i thread
   osThreadNew(TaskBlink6, NULL, &blinkAttr);  // 2) crea i thread
+  osThreadNew(TaskBlink7, NULL, &blinkAttr);  // 2) crea i thread
   osKernelStart(); 
   for(;;) {}
 }
